46a54ad27befc5a692ab47dbd1fafc675f7613d7
VitorFCM <vitor.carreramiguel@gmail.com>
2023-05-15 21:20:37 -0300

    Moving the shutdown code to _start function

--- C/hw_start.c
+++ C/hw_start.c
@@ -4,6 +4,11 @@ void __attribute__((naked)) _start(){
 	__asm__(
 		"	mov 	$__END_STACK__,	%sp	\n"
 		"	call 	main			\n"
+		"shutdown:                              \n"
+		"	movw    $0x5307, %ax            \n"
+		"	movw    $0x0001, %bx            \n"
+		"	movw    $0x0003, %cx            \n"
+		"	int     $0x15                   \n"
 		"halt:					\n"
 		"	hlt				\n"
 		"	jmp	halt			\n"
